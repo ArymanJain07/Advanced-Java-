c508678af58aeb869d81f4640a4a676c0fab4042
Aryman Jain <92370261+ArymanJain07@users.noreply.github.com>
2024-06-26 09:31:17 +0530

    Add files via upload

---
+++ HibernateMappingOneToMany/src/main/java/com/demo/beans/Student.java
@@ -0,0 +1,56 @@
+package com.demo.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+@Entity
+public class Student {
+
+	@Id
+	private int sid;
+	private String sname;
+
+	@OneToMany
+	private List<Laptop> laptop = new ArrayList<Laptop>();
+
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public List<Laptop> getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(List<Laptop> laptop) {
+		this.laptop = laptop;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", laptop=" + laptop + "]";
+	}
+
+	public Student(int sid, String sname, List<Laptop> laptop) {
+		super();
+		this.sid = sid;
+		this.sname = sname;
+		this.laptop = laptop;
+	}
+	
+	
+}
---
+++ HibernateMappingOneToMany/src/main/java/com/demo/test/OneToManyTest.java
@@ -0,0 +1,38 @@
+package com.demo.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.demo.beans.Laptop;
+import com.demo.beans.Student;
+
+public class OneToManyTest {
+
+	public static void main(String[] args) {
+		
+		Configuration con = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
+		SessionFactory sf = con.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tr = session.beginTransaction();
+		Laptop lap = new Laptop(2,"del");
+		Laptop lap2 = new Laptop(3,"mac");
+		
+		List<Laptop> lList = new ArrayList<Laptop>();
+		lList.add(lap);
+		lList.add(lap2);
+		Student stu = new Student(2,"dhruv",lList);
+		
+		session.save(stu);
+		session.save(lap);
+		session.save(lap2);
+		
+		tr.commit();
+		session.clear();
+	}
+
+}
